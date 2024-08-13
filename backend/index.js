7082246bbd4b2b21ba0ddf6de6bb6629883adcff
Vinuk Lakvindu <it22055026@my.sliit.lk>
2024-08-13 11:42:18 +0530

    configer the backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,26 @@
-// select * as fs from 'fs';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-console.log('Vinuk');
-console.log('lakvindu');
-console.log('gamage');
+dotenv.config();
+
+const app  = express();
+const PORT = process.env.port || 5000;
+
+const mongoURI = process.env.MONGODB_URL;
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+
+app.listen(PORT, () => console.log(`Listening on: ${PORT}`));
+
+
